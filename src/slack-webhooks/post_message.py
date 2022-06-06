9dcb989ef2fd9cc3a34d036a81bc5fd7e7827f9b
Kristoffer Nordström <kristoffer.nordstrom@northerntest.se>
2022-06-06 15:11:23 +0200

    chore: removed webhook

--- src/slack-webhooks/post_message.py
+++ src/slack-webhooks/post_message.py
@@ -5,4 +5,4 @@ SLACK_WEBHOOK = os.getenv('SLACK_WEBHOOK')
 
 headers = {'Content-type': 'application/json'}
 
-requests.post('https://hooks.slack.com/services/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', headers=headers, json={"text":"Hello, World!"})
+requests.post(WEBHOOK, headers=headers, json={"text":"Hello, World!"})
