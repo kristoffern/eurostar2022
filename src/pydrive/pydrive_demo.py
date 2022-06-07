d48668b762da1b29dd36056241ed7d11d6fed33d
Kristoffer Nordstrom <kristoffer.nordstrom@northerntest.se>
2022-06-07 15:53:48 +0200

    ore: update GDrive example to use PyDrive2

--- src/pydrive/pydrive_demo.py
+++ src/pydrive/pydrive_demo.py
@@ -1,22 +1,27 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+"""
+    Example code showing how you can authenticate with a json credential file
+    from a service account in Google Cloud.
+
+"""
+
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
 
 gauth = GoogleAuth()
 scope = ["https://www.googleapis.com/auth/drive"]
-gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secrets.json', scope)
+gauth.auth_method = 'service'
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('eurostar-2022-1cf61be6884d.json', scope)
 drive = GoogleDrive(gauth)
 
-print(drive)
-
-file1 = drive.CreateFile({'title': 'testdata.tar.gz', 'mimeType': 'application/gzip'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-file1.SetContentFile('testdata.tar.gz') # Set content of the file from given string.
+file1 = drive.CreateFile({'title': 'reports.zip', 'mimeType': 'application/zip'})
+file1.SetContentFile('reports.zip')
 file1.Upload()
 
-# Insert the permission.
-permission = file1.InsertPermission({
-                        'type': 'anyone',
-                        'value': 'anyone',
-                        'role': 'reader'})
+file1.InsertPermission({
+    'type': 'anyone',
+    'value': 'anyone',
+    'role': 'reader'
+    })
 
-print(file1['alternateLink'])  # Display the sharable link.
+print(file1['alternateLink'])
